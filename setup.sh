f285f5a41afb06f6a00f0733d10a4d073a37782e
Cameron Embree <CSEmbree@gmail.com>
2013-10-13 10:13:07 -0700

    execute script to compile

---
+++ setup.sh
@@ -0,0 +1,8 @@
+ #!/bin/bash          
+
+#DEBUG ON
+#gcc -std=c99 -g -DDEBUG=0 sortcmp.c quick.c merge.c insert.c -o sortcmp
+
+
+#DEBUG OFF
+gcc -std=c99 -g -DDEBUG=1 sortcmp.c quick.c merge.c insert.c -o sortcmp
